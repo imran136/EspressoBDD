a01977e0cade6f2bffd0207a892c7a92f0559a68
Imran <imran136@gmail.com>
2021-12-19 15:11:41 +0100

    Some refactoring

--- app/src/androidTest/java/com/example/myfinalapp/test/steps/LoginActivitySteps.kt
+++ app/src/androidTest/java/com/example/myfinalapp/test/steps/LoginActivitySteps.kt
@@ -4,7 +4,6 @@ package com.example.myfinalapp.test.steps
 import androidx.test.rule.ActivityTestRule
 import com.example.myfinalapp.MainActivity
 import com.example.myfinalapp.test.screens.LoginScreen
-import com.example.myfinalapp.test.util.ActivityFinisher
 import cucumber.api.java.After
 import cucumber.api.java.Before
 import cucumber.api.java.en.And
@@ -15,7 +14,7 @@ import cucumber.api.java.en.When
 
 class LoginActivitySteps {
 
-    private val robot = LoginScreen()
+    private val loginScreen = LoginScreen()
 
     private val activityRule = ActivityTestRule(MainActivity::class.java, false, false)
 
@@ -26,47 +25,48 @@ class LoginActivitySteps {
 
     @After
     fun tearDown() {
-        ActivityFinisher.finishOpenActivities() // Required for test scenarios with multiple activities or scenarios with more cases
+        activityRule.finishActivity()
+        //ActivityFinisher.finishOpenActivities() // Required for test scenarios with multiple activities or scenarios with more cases
     }
 
     @Given("^I start the application$")
     fun i_start_app() {
-        robot.launchLoginScreen(activityRule)
+        loginScreen.launchLoginScreen(activityRule)
     }
 
     @When("^I click email field$")
     fun i_click_email_field() {
-        robot.selectEmailField()
+        loginScreen.selectEmailField()
     }
 
     @And("^I close the keyboard$")
     fun i_close_the_keyboard() {
-        robot.closeKeyboard()
+        loginScreen.closeKeyboard()
     }
 
     @And("^I enter valid email (\\S+)$")
     fun i_enter_valid_email(email: String) {
-        robot.enterEmail(email)
+        loginScreen.enterEmail(email)
     }
 
     @And("^I click password field$")
     fun i_click_password_field() {
-        robot.selectPasswordField()
+        loginScreen.selectPasswordField()
     }
 
     @And("^I enter valid password (\\S+)$")
     fun i_enter_valid_password(password: String) {
-        robot.enterPassword(password)
+        loginScreen.enterPassword(password)
     }
 
     @And("^I click sign in button$")
     fun i_click_sign_in_button() {
-        robot.clickSignInButton()
+        loginScreen.clickSignInButton()
     }
 
     @Then("^I expect to see successful login message$")
     fun i_expect_to_see_successful_login_message() {
-        robot.isSuccessfulLogin()
+        loginScreen.isSuccessfulLogin()
     }
 
 }
